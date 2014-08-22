a8331f6d2da389c3aae5b393e06e35b098529d59
Scott Weston <weston.scot@gmail.com>
2014-08-22 17:38:12 +1200

    Fixed up the last of the naive bayes classifier

--- NaiveBayes.hs
+++ NaiveBayes.hs
@@ -1,4 +1,21 @@
---module NaiveBayes where
+module NaiveBayes(
+    Feature,
+    Class,
+    Instance (Instance),
+    TrainingSet,
+    gaussianValue,
+    mean,
+    variance,
+    standardDeviation,
+    getFeature,
+    classProbability,
+    classify,
+    normalizeProb
+    ) where
+    
+import qualified Data.Set as Set
+import qualified Data.List as List
+import Data.Function
 
 type Feature = Double
 type Class = String
@@ -33,6 +50,9 @@ standardDeviation = sqrt . variance
 getFeature :: Int -> Instance -> Feature
 getFeature index (Instance _ features) = features !! index
 
+getClass :: Instance -> Class
+getClass (Instance c _) = c
+
 meanFeature :: TrainingSet -> Int -> Double
 meanFeature set index = mean . map (getFeature index) $ set
 
@@ -50,23 +70,27 @@ conditionProbabilityDensity :: TrainingSet -> Double -> Int -> Class -> Double
 conditionProbabilityDensity set testValue index c = gaussianValue (meanFeature dat index) (varFeature dat index) testValue 
     where dat = filter (isClass c) $ set
 
-probablityClass :: TrainingSet -> Class -> Double
-probablityClass set c = (numLength dat) / (numLength set)
+classProbability :: Class -> TrainingSet -> Double
+classProbability c set = (numLength dat) / (numLength set)
     where dat = filter (isClass c) $ set
 
 getClasses :: TrainingSet -> [Class]
-getClasses set = []
+getClasses = Set.toList . foldr Set.insert Set.empty . map getClass
+
+getFeatures :: Instance -> [Feature]
+getFeatures (Instance _ feat) = feat
+
+normalize :: (Floating a) => [a] -> [a]
+normalize xs = map (* factor) xs
+    where total = sum xs 
+          factor = 1 / total
+
+normalizeProb :: [(Class, Double)] -> [(Class, Double)]
+normalizeProb probs = zip (map fst probs) . normalize . map snd $ probs
 
 classify :: TrainingSet -> Instance -> [(Class, Double)]  --We return a mapping from class to probablity
-classify set inst = []
-
-trSet :: TrainingSet
-trSet = [Instance "male"    [6,    180, 12],
-         Instance "male"    [5.92, 190, 11],
-         Instance "male"    [5.58, 170, 12],
-         Instance "male"    [5.92, 165, 10],
-         Instance "female"  [5,    100, 6],
-         Instance "female"  [5.5,  150, 8],
-         Instance "female"  [5.42, 130, 7],
-         Instance "female"  [5.75, 150, 9]]
+classify set (Instance _ features) = List.sortBy (\(_, v1) (_, v2) -> compare v2 v1) . map (\cl -> (cl, posteriorNumerator cl)) $ classes
+    where classes = getClasses set
+          posteriorNumerator cl = (*) (classProbability cl set) . foldr (*) 1 . foldr (\x acc -> featureProbability x cl : acc) [] . zip [0..] $ features
+          featureProbability (index, featureValue) cl = conditionProbabilityDensity set featureValue index cl 
 
